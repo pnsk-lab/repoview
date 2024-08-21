2075b95b6b6440d08c5124627a9321436f13ba2d
Nishi <nishi@nishi.boats>
2024-08-21 17:12:46 +0000

    fix
    git-svn-id: file:///raid/svn-personal/repoview/trunk@26 7e8b2a19-8934-dd40-8cb3-db22cdd5a80f

--- Platform/linux.mk
+++ Platform/linux.mk
@@ -1,6 +1,6 @@
 # $Id: generic.mk 3 2024-08-20 21:05:24Z nishi $
 
 CC = cc
-CFLAGS = -std=c99 -D_DEFAULT_SOURCE
+CFLAGS = -std=c99 -D_DEFAULT_SOURCE -I /usr/include/GraphicsMagick
 LDFLAGS =
 LIBS = -lcrypto
